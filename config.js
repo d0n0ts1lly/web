6cdbd57956d3dde20345f71f1638e001ddac9b0d
Badun Ivan <d0n0ts1lly@MacBook-Air-Badun.local>
2026-04-13 09:51:31 +0300

    new config

--- config.js
+++ config.js
@@ -2,9 +2,13 @@ export const CONFIG = {
   PORT: 3000,
   DATA_PATH: "./src/data/events.json",
   DB: {
-    host: "localhost",
-    user: "root",
-    password: "23032023",
-    database: "marathon_db",
+    host: "mysql-336c7cbf-ivanbadun14032007-2ad4.g.aivencloud.com",
+    port: 22078,
+    user: "avnadmin",
+    password: "AVNS_7E1y2ycworBSFriyXQ6",
+    database: "defaultdb",
+    ssl: {
+      rejectUnauthorized: false,
+    },
   },
 };
