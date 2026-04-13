aee4f4f3dc5ac05842e1a03623e6160cf5f0f90f
Badun Ivan <d0n0ts1lly@MacBook-Air-Badun.local>
2026-04-13 10:54:39 +0300

    chore: setup database env vars and fix dependencies

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
 export const CONFIG = {
-  PORT: 3000,
+  PORT: process.env.PORT || 3000,
   DATA_PATH: "./src/data/events.json",
   DB: {
-    host: "localhost",
-    user: "root",
-    password: "23032023",
-    database: "marathon_db",
+    host: process.env.MYSQLHOST || "localhost",
+    user: process.env.MYSQLUSER || "root",
+    password: process.env.MYSQLPASSWORD || "23032023",
+    database: process.env.MYSQLDATABASE || "marathon_db",
+    port: process.env.MYSQLPORT || 3306,
   },
 };
